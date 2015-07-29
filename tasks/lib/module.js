b5a74c985a6c84a721f5d1cca11e4b6a069ecbdf
pauojm <pauojm@pauojm-K46CB>
2015-07-28 22:52:58 -0300

    add module lib

---
+++ tasks/lib/module.js
@@ -0,0 +1,42 @@
+
+
+/* global options */
+
+/* global options */
+
+/* global exports */
+
+exports.init = function (grunt) {
+
+    'use strict';
+
+    var path = require('path');
+    
+    var exports = {};
+
+    (function () {
+        
+        var app = angular.module("[moduleName]", []);
+        
+        //Services
+        [services]
+        
+        //controllers
+        [controllers]
+        
+        //directives
+        [directives]
+        
+        //directives
+        [directives]
+        
+        //views
+        [views]
+        
+    }());
+    
+    exports.createModule = function(){};
+    
+    return exports;
+    
+};
